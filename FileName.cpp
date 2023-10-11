253f70700a874edf0fd68b92e7480fe19d94d6f9
hunchakivan <hunchak.ivan84@gmail.com>
2023-10-11 10:39:35 +0300

    lab_03.2

---
+++ FileName.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	double x;
+	double a;
+	double b;
+	double c;
+	double F;
+
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+
+	if (x < 0 && b != 0)
+		F = (a - x) / (10 + b);
+	if (x > 0 && b == 0)
+		F = (x - a) / (x - c);
+	if (!(x < 0 && b != 0) && !(x > 0 && b == 0))
+		F = 3 * x + 2 / c;
+
+	cout << endl;
+	cout << "1) F = " << F << endl;
+
+	if (x < 0 && b != 0)
+		F = (a - x) / (10 + b);
+	else
+		if (x > 0 && b == 0)
+			F = (x - a) / (x - c);
+		else
+			F = 3 * x + 2 / c;
+
+	cout << "2) F = " << F << endl;
+
+	cin.get();
+	return 0;
+}
